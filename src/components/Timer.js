19f1f5d850399cfe3da261e566e20317ef5540e5
Nicolas <nickov.nv@gmail.com>
2022-02-28 14:58:23 -0300

    initial commit

---
+++ src/components/Timer.js
@@ -0,0 +1,14 @@
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
+const red = '#f54e4e';
+const green = '#4aec8c';
+
+function Timer(){
+    return (
+        <div>
+            <CircularProgressbar value={60} text={`60%`} styles={buildStyles({textColor: '#fff', pathColor: red, tailColor: 'rgba(255,255,255, .2)'})} />
+        </div>
+    )
+}
+export default Timer;
