014dd24e3f636d1f3b50366f6ec90ff7dee0c83c
Nicolas <nickov.nv@gmail.com>
2022-02-28 18:54:55 -0300

    added import changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 
 import './App.css';
+import Settings from './components/Settings';
 import Timer from './components/Timer';
+import {useState} from "react";
+import SettingsContext from './components/SettingsContext';
 
 function App() {
+  const [showSetting, setShowSetting] = useState(false);
+  const [workMinutes, setWorkMinutes] = useState(45);
+  const [breakMinutes, setBreakMinutes] = useState(15);
+
   return (
     <main>
-      <Timer/>
+      <SettingsContext.Provider value={{
+        showSetting,
+        setShowSetting,
+        workMinutes,
+        breakMinutes,
+        setWorkMinutes,
+        setBreakMinutes,
+      }}>
+        {showSetting ? <Settings/> : <Timer/>}
+      </SettingsContext.Provider>
     </main>  
   );
 }
